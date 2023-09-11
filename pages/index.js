7701479d1868aabf7c039e6012845d9b0d5b9e4f
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-09-11 16:13:54 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Context} from '../context';
+import {useRouter} from 'next/router';
+import axios from 'axios';
+
+export default function Auth() {
+  return (
+    <div className="background">
+      <div className='auth-container'>
+        <form className='auth-form'
+              onSubmit={(e) =>
+                e.preventDefault()}>
+        </form>
+      </div>
+    </div>
+  )
+}
