6a42702013caa3c7a1928b0540aa35abd22fe102
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-09-11 17:44:33 +0300

    protectId

--- pages/chats.js
+++ pages/chats.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from 'react'
+import { Context } from '@/context'
+import {useRouter} from 'next/router';
+import dynamic from 'next/dynamic';
+
+
+const ChatEngine = dynamic(() =>
+   import("react-chat-engine").then((module) => module.ChatEngine)
+);
+
+const MessageFormSocial = dynamic(() =>
+  import("react-chat-engine").then((module) => module.MessageFormSocial)
+);
+
 
 export default function Chats() {
-  return <div className="background">chats</div>;
+  const {username, secret} = useContext(Context);
+  const [showChat, setShowChat] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof  document !== null) {
+      setShowChat(true)
+    }
+  });
+
+  if (!showChat) return <div />;
+
+  return (
+      <div className="background">
+        <div className='shadow'>
+          <ChatEngine
+          height='calc(100ch - 200px'
+          protectID='789eb031-fcfd-4aba-9be3-228b67575ee9'
+          userName={username}
+          userSecret={secret}
+          />
+        </div>
+
+      </div>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
 import React, { useContext } from 'react'
-import {Context} from '../context';
 import {useRouter} from 'next/router';
 import axios from 'axios';
+import { Context } from '@/context'
 
 export default function Auth() {
+  const {username, setUsername, secret, setSecret} = useContext(Context);
+  const router = useRouter()
 
-  const { setUsername, setSecret} = useContext(Context);
+  function onSubmit(e) {
+    e.preventDefault();
+    if(username.length === 0 || secret.length === 0) return
+
+    axios
+      .put(
+      'https://api.chatengine.io/users/',
+      {username, secret},
+           {headers: {"Private-key": "4c2ab7cb-31a9-406c-ae6c-829efcddcaa5"}}
+    )
+      .then((r) => router.push("/chats"));
+  }
 
   return (
     <div className="background">
       <div className='auth-container'>
         <form className='auth-form'
-              onSubmit={(e) =>
-                e.preventDefault()}>
+              onSubmit={(e) => onSubmit(e)}>
         </form>
         <div className='auth-title'>Grozny Chat on NextJS</div>
         <div className='input-container'>
